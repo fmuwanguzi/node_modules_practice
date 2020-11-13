cdc0b5e8540fa94f174713286960c82f238941e7
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2020-11-12 19:43:39 -0500

    [add gitignore and tried the weather npm]

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const { foods } = require('./myModule')
 
-foods.forEach(food => console.log(food));
+//Array of foods from myModule
+foods.forEach(fav => console.log(fav));
+
+//weather npm https://www.npmjs.com/package/weather
+const Weather = require('weather');
+
+const appID = 'W5yFJZ4yJG94Z4V2wRIV';
+const appCode = 'PiUk1t1TckSwxsiq23B6IQ';
+
+const weather = new Weather({
+    appID,
+    appCode
+});
+
+//tells you the weather anywhere just change the location 
+weather.now('Kampala, Uganda').then((results) => {
+    console.log(results);
+});
+
+weather.now('Kampala, Uganda').then((results) => {
+    console.log(results);
+});
--- myModule.js
+++ myModule.js
@@ -1,12 +1,7 @@
 const foods = ["wings", "salad", "dependsOntheDay"];
 
-// function myArray(foods){
-//     forEach(foods){
-//         return foods[i];
-//     }
-// }
 
 module.exports = {
-    // myArray,
+
     foods,
 }
